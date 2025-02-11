f3b46f94bd4124dc0cade174afbf3bba8b01a9c1
JamesKibathi <knjenga94@gmail.com>
2025-02-11 12:29:03 +0300

    Index the products table

--- api/models/products.py
+++ api/models/products.py
@@ -11,5 +11,14 @@ class Product(BaseModel):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
 
+    class Meta:
+        verbose_name = "product"
+        verbose_name_plural = "products"
+        ordering = ['name'] 
+        indexes = [
+            models.Index(fields=['name']),  
+            models.Index(fields=['category']),
+        ]
+
     def __str__(self):
         return self.name
